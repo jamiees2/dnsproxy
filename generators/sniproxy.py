1af14d6f528b63ccbd94b52048076a2674188d77
matthias1232 <matthias.1232@gmail.com>
2022-07-24 18:52:26 +0200

    Update sniproxy.py

--- generators/sniproxy.py
+++ generators/sniproxy.py
@@ -117,7 +117,7 @@ def generate(config, dnat=False):
     haproxy_catchall_backend_content = generate_backend('catchall', 'http', None, None, None, True)
     for group in config["groups"].values():
         for proxy in group["proxies"]:
-            haproxy_catchall_backend_content += generate_backend_catchall_entry(proxy["domain"], protocol, port(protocol), server_options)
+            haproxy_catchall_backend_content += generate_backend_catchall_entry(proxy["domain"])
     
     sniproxy_content = generate_startconfig01()
 
