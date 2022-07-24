420616a5784b5b8254d05f44852cde339a2358e6
matthias1232 <matthias.1232@gmail.com>
2022-07-24 19:48:51 +0200

    Update sniproxy.py

--- generators/sniproxy.py
+++ generators/sniproxy.py
@@ -94,10 +94,9 @@ def generate(config, dnat=False):
     sniproxy_content += generate_hosts01()    
 
     for group in config["groups"].values():
-        c = chunks([proxy["domain"] for proxy in group["proxies"]], 5)
+        sniproxy_content += generate_dns(group)
 
-        for chunk in c:
-            sniproxy_content += generate_dns(chunk)
+
 
     sniproxy_content += generate_hosts02()  
 
