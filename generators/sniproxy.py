456d57a63ca0dc4a052508b87c2e38e7d97009e7
matthias1232 <matthias.1232@gmail.com>
2022-07-24 18:49:34 +0200

    Update sniproxy.py

--- generators/sniproxy.py
+++ generators/sniproxy.py
@@ -119,10 +119,8 @@ def generate(config, dnat=False):
             if not dnat or (dnat and not proxy["dnat"]):
                 for protocol in proxy["protocols"]:
                     if protocol == 'http':
-                        haproxy_catchall_frontend_content += generate_frontend_catchall_entry(proxy["domain"], protocol)
                         haproxy_catchall_backend_content += generate_backend_catchall_entry(proxy["domain"], protocol, port(protocol), server_options)
                     elif protocol == 'https':
-                        haproxy_catchall_frontend_ssl_content += generate_frontend_catchall_entry(proxy["domain"], protocol)
                         haproxy_catchall_backend_ssl_content += generate_backend_catchall_entry(proxy["domain"], protocol, port(protocol), server_options)
     
     sniproxy_content = generate_startconfig01()
