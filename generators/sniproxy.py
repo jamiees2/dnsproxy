6b1c656f10291002ea69f648d5b61c0a1bbe3297
matthias1232 <matthias.1232@gmail.com>
2022-07-24 19:40:24 +0200

    Update sniproxy.py

--- generators/sniproxy.py
+++ generators/sniproxy.py
@@ -69,7 +69,7 @@ def generate_listentls():
 
 def generate_dns(dest_addrs):
     if isinstance(dest_addrs, list):
-        result = 'address=/' + dest_addrs + '/'
+        result = 'address=/' + str(dest_addrs) + '/'
     else:
         result = 'address=/' + dest_addrs + '/'
     return result + os.linesep
