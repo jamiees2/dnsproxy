41c7f8f6ac7d003264cafa3294b399e14faa508e
matthias1232 <matthias.1232@gmail.com>
2022-07-24 19:27:15 +0200

    Update sniproxy.py

--- generators/sniproxy.py
+++ generators/sniproxy.py
@@ -69,7 +69,7 @@ def generate_listentls():
 
 def generate_dns(dest_addrs, current_ip):
     if isinstance(dest_addrs, list):
-        result = 'address=/' + dest_addrs + '/' + current_ip
+        result = 'address=/' + "/".join(dest_addrs) + '/' + current_ip
     else:
         result = 'address=/' + dest_addrs + '/' + current_ip
     return result + os.linesep
@@ -100,23 +100,13 @@ def generate(config, dnat=False):
     current_ip = config["base_ip"]
 
     for group in config["groups"].values():
-        if not dnat:
-            c = chunks([proxy["domain"] for proxy in group["proxies"]], 5)
-        else:
-            c = chunks([proxy["domain"] for proxy in group["proxies"] if not proxy["dnat"]], 5)
+        c = chunks([proxy["domain"] for proxy in group["proxies"]], 5)
 
         for chunk in c:
             if not dnat:
                 sniproxy_content += generate_dns(chunk, public_ip)
             else:
                 sniproxy_content += generate_dns(chunk, current_ip)
-
-    if dnat:
-        for group in config["groups"].values():
-            for proxy in group["proxies"]:
-                if proxy["dnat"]:
-                    current_ip = long2ip(ip2long(current_ip) + 1)
-                    sniproxy_content += generate_dns(proxy["domain"], current_ip)
 
 
 
